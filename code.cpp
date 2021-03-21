f4f5897956e12261d7d81a47a7c6fb9ecc17c4c1
Arif733 <81081788+Arif733@users.noreply.github.com>
2021-03-21 22:45:37 +0600

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,16 @@
+##### Elements of array using while loop
+
+#include <iostream>
+using namespace std;
+int main(){
+   int arr[]={21,87,15,99, -12};
+   /* The array index starts with 0, the
+    * first element of array has 0 index
+    * and represented as arr[0]
+    */
+   int i=0;
+   while(i<5){
+      cout<<arr[i]<<endl;
+      i++;
+   }
+}
